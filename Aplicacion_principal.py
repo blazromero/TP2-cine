bb99fa49550a39084c287b431177577836f28ba7
blazromero <baromero@fi.uba.ar>
2023-11-19 16:56:01 -0300

    diseño de ventana y cargamos snacks

---
+++ Aplicacion_principal.py
@@ -0,0 +1,103 @@
+from tkinter import Tk, Entry, Button, Label, Frame, LabelFramer
+import requests
+
+CARPETA_CODIGOS_QR: str = 'QR'
+ACLARACION_FUNCIONES_PELICULAS: str = 'Cada cine tiene sala unica. Cada pelicula se proyectara en la misma sala pero distintas horas y dias para evitar superposicion.'
+ACLARACION_FONT: str = 'Helvetica 9 bold'
+PRECIO_ENTRADAS_GENERAL: float = 3000.0
+MAX_ENTRADAS: int = 12
+MIN_ENTRADAS: int = 1
+
+MAX_SNACKS: int = 12
+
+CHECKOUT_FONT: str = 'Helvetica 9 bold'
+
+TOKEN: str = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.DGI_v9bwNm_kSrC-CQSb3dBFzxOlrtBDHcEGXvCFqgU'
+API: dict = {
+    'BASE_URL': 'http://vps-3701198-x.dattaweb.com:4000',
+    'PATH': {
+        'TODAS_LAS_PELIS': '/movies',
+        'PELI_POR_ID': '/movies/{movie_id}',
+        'POSTER_POR_ID': '/posters/{poster_id}',
+        'TODOS_LOS_SNACKS': '/snacks',
+        'DONDE_SE_PROYECTA': '/movies/{movie_id}/cinemas',
+        'TODOS_LOS_CINES': '/cinemas',
+        'TODAS_LAS_PELIS_DEL_CINE': '/cinemas/{cinema_id}/movies'
+    },
+    'HEADERS': { 'Authorization': f'Bearer {TOKEN}'}
+}
+
+def mostrar_ventana_secundaria(totem)-> None:
+    ventana_secundaria = Tk()
+    ventana_secundaria.geometry('400x800')
+    ventana_secundaria.title('Pelicula')
+    ventana_secundaria.config(bg= 'black')
+
+    ventana_secundaria.mainloop()
+def diseño_pantalla_principal(pantalla_principal, totem)-> dict:
+    pantalla_principal = pantalla_principal
+
+    
+    lbl_cines = Label(text = 'Cine : ', bg= 'black', fg= 'white' )
+    lbl_cines.grid(row=0, column=1)
+    
+    lbl_pelicula = Label(text = 'Buscar peli: ', bg= 'black', fg= 'white' )
+    lbl_pelicula.grid(row=1, column=1)
+    
+
+    lbl_espacios = Label(text='   adsdsadss           ', bg= 'black')
+    lbl_espacios.grid(row=3, column=0)
+    lbl_espacios1 = Label(text='as ', bg= 'black')
+    lbl_espacios1.grid(row=4, column=0)
+    lbl_espacios2 = Label(text='s', bg= 'black')
+    lbl_espacios2.grid(row=5, column=0)
+    lbl_espacios3 = Label(text='sa', bg= 'black')
+    lbl_espacios3.grid(row=6, column=0)
+    
+    
+
+    lbl_aclaracion: Label = Label(pantalla_principal, text=ACLARACION_FUNCIONES_PELICULAS, wraplength=200, bg='CadetBlue', font=ACLARACION_FONT)
+    lbl_aclaracion.grid(row=2, column=3, columnspan=2, sticky='news', pady=20)
+    entrada_usuario = Entry(pantalla_principal, width= 34)
+    entrada_usuario.grid(row=1, column=3)
+
+    
+def cargar_snacks(totem: dict) -> None:
+    '''
+    PRE: Se esperan los parametros solicitado de forma correcta.
+    POST: Se actualiza el estado de snacks, cargandolo con los datos obtenidos de la API dada.
+    '''
+    snacks: dict = {}
+
+    try:
+        request_todos_los_snakcs = requests.get(API['BASE_URL'] + API['PATH']['TODOS_LOS_SNACKS'], headers=API['HEADERS'])
+    except Exception as e:
+        print('Error al consultar API request_todos_los_snakcs', str(e))
+
+    if request_todos_los_snakcs.status_code == 200:
+        snacks = request_todos_los_snakcs.json()
+
+    totem['SNACKS'] = snacks
+
+
+
+
+
+
+
+
+
+def main() -> None:
+    totem:dict = {}
+    
+    pantalla_principal = Tk()
+    pantalla_principal.geometry('800x1500')
+    pantalla_principal.title('Pantalla Principal')
+    pantalla_principal.config(bg = 'black')
+
+    diseño_pantalla_principal(pantalla_principal, totem)
+    
+    pantalla_principal.mainloop()
+
+
+main()
